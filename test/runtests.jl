aacda2d3129a7c93fba792a5afbea0dfc624692e
Maarten Pronk <git@evetion.nl>
2022-06-05 22:54:04 +0200

    Try to debug Parquet2 filepath bug on several platforms.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,8 @@ using JSON3
 import GeoFormatTypes as GFT
 
 for url in (
-    "https://github.com/opengeospatial/geoparquet/raw/v0.4.0/examples/example.parquet",
-    "https://storage.googleapis.com/open-geodata/linz-examples/nz-buildings-outlines.parquet"
+# "https://github.com/opengeospatial/geoparquet/raw/v0.4.0/examples/example.parquet",
+# "https://storage.googleapis.com/open-geodata/linz-examples/nz-buildings-outlines.parquet"
 )
     fn = joinpath("data", basename(url))
     isfile(fn) || @info "Downloading " * Downloads.download(url, fn)
@@ -18,39 +18,48 @@ end
 @testset "GeoParquet.jl" begin
     # Write your tests here.
 
-    @testset "Reading" begin
-        fn = "data/example.parquet"
-        ds = Parquet2.Dataset(fn)
-        meta = GeoParquet.geometadata(ds)
-        @test meta.version == "0.4.0"
-        @test meta.columns["geometry"].bbox[end] ≈ 83.6451
+    # @testset "Reading" begin
+    #     fn = "data/example.parquet"
+    #     ds = Parquet2.Dataset(fn)
+    #     meta = GeoParquet.geometadata(ds)
+    #     @test meta.version == "0.4.0"
+    #     @test meta.columns["geometry"].bbox[end] ≈ 83.6451
 
-        ds = Parquet2.Dataset("data/nz-buildings-outlines.parquet")
-        # this file is still at 0.1.0, using "schema_version", breaking our code
-        # @info Parquet2.metadata(ds)["geo"]
-        # meta = GeoParquet.geometadata(ds)
-        # @info meta
-        # @test meta.version == "0.3.0"
-        # @test meta.columns["geometry"].bbox[end] ≈ 6190596.9
+    #     ds = Parquet2.Dataset("data/nz-buildings-outlines.parquet")
+    #     # this file is still at 0.1.0, using "schema_version", breaking our code
+    #     # @info Parquet2.metadata(ds)["geo"]
+    #     # meta = GeoParquet.geometadata(ds)
+    #     # @info meta
+    #     # @test meta.version == "0.3.0"
+    #     # @test meta.columns["geometry"].bbox[end] ≈ 6190596.9
 
-        df = GeoParquet.read(fn)
-        @test nrow(df) === 5
-        @test df.geometry[1] isa GFT.WellKnownBinary
+    #     df = GeoParquet.read(fn)
+    #     @test nrow(df) === 5
+    #     @test df.geometry[1] isa GFT.WellKnownBinary
 
-        @test_throws Exception GeoParquet.read(fn, columns=(:geom,))
-    end
+    #     @test_throws Exception GeoParquet.read(fn, columns=(:geom,))
+    # end
 
     @testset "Writing" begin
-        geom = ArchGDAL.createpoint.([[1, 2], [1, 2]])
-        wkb = ArchGDAL.toWKB.(geom)
-        df = DataFrame(test="test", value=rand(2), geometry=wkb)
-        fn = "data/write.parquet"
-        GeoParquet.write(fn, df, (:geometry,))
-        df = GeoParquet.read(fn)
-        df.test[1] == "test"
+        # geom = ArchGDAL.createpoint.([[1, 2], [1, 2]])
+        # wkb = ArchGDAL.toWKB.(geom)
+        # df = DataFrame(test="test", value=rand(2), geometry=wkb)
+        # fn = "data/write.parquet"
+        # GeoParquet.write(fn, df, (:geometry,))
+        # df = GeoParquet.read(fn)
+        # df.test[1] == "test"
 
-        fn = "data/example.parquet"
-        df = GeoParquet.read(fn)
-        GeoParquet.write("data/example_copy.parquet", df, (:geometry,))
+        # fn = "data/example.parquet"
+        # @time df = GeoParquet.read(fn)
+        # @time GeoParquet.write("data/example_copy.parquet", df, (:geometry,))
+    end
+
+    @testset "Parquet2 FilePath" begin
+        df = DataFrame(a=1)
+        Parquet2.writefile("test.parquet", df)
+        mv("test.parquet", "test2.parquet")
+        ds = Parquet2.Dataset("test2.parquet")
+        DataFrame(ds)
+
     end
 end
